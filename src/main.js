a9d769ba33386affa17f2f4d9280969706f9eb1a
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-10 14:31:31 -0800

    Simple layout for image, sidebar menu hamburger, image container resizes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './style.css'
 
-const element = document.createElement('div');
-element.textContent = "Hello there";
-document.body.append(element);
+const hamburgerMenu = document.querySelector('.hamburger');
+const mainDisplay = document.querySelector('#mainDisplay');
+const sideMenu = document.querySelector('#sidebar');
+const sliderContainer = document.querySelector('#sliderContainer');
 
+
+
+hamburgerMenu.addEventListener('click', ()=>{
+    sideMenu.classList.toggle('visible');
+    mainDisplay.classList.toggle('menuIsHere');
+    sliderContainer.classList.toString('sidebarToggled');
+    console.log('Hello there');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: './src/main.js',
-
+    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
